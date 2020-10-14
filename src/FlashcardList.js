505cb7db5197d626a1df6647d769bd5f5d0125cf
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-10-14 20:44:36 +0630

    initial commit

---
+++ src/FlashcardList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Flashcard from './Flashcard'
+
+const FlashcardList = ({flashcards}) => {
+  return (
+    <div className="card-grid">
+      {flashcards.map(flashcard =>{
+          return <Flashcard key={flashcard.id} flashcard={flashcard} />
+      })}
+    </div>
+  )
+}
+
+export default FlashcardList
